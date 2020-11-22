655e021ab43125a5c2d577c8fd08f6143154151d
AOCAWOL <51396466+AOCAWOL@users.noreply.github.com>
2020-11-22 00:23:23 -0700

    -added frost & shroomlight essence

--- src/main/java/AOChips/ArmorUp/lists/ItemList.java
+++ src/main/java/AOChips/ArmorUp/lists/ItemList.java
@@ -7,5 +7,7 @@ public class ItemList {
     public static Item FORGING_TABLE;
     public static Item ESSENCE_BOTTLE;
     public static Item SOUL_FIRE_ESSENCE;
+    public static Item SHROOMLIGHT_ESSENCE;
+    public static Item FROST_ESSENCE;
 
 }
